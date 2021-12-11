39451df9ae2a0c434177398636845336cae1eb71
mahamud abdi <mahamud0660@gmail.com>
2021-12-11 16:37:55 +0300

    kaabe

--- kaabe_portfolio/wsgi.py
+++ kaabe_portfolio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaabe_portfolio.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaabe_portfolio.settings.prod')
 
 application = get_wsgi_application()
